934900461055323568af3622b39e19edfefa9822
Devin <demitchell14@gmail.com>
2019-05-06 09:13:38 -0500

    .

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,28 @@
 import * as path from "path";
 import * as fs from "fs";
+import git from "gift";
 
 const root = process.cwd();
 
 const packageFile = path.join(root, "package.json");
 
-const packageJson = fs.readFileSync(packageFile).toString("UTF-8");
+const packageJson = JSON.parse(fs.readFileSync(packageFile).toString("UTF-8"));
 
-console.log(JSON.parse(packageJson));
+const repoLocation = packageJson.repository;
+const version = packageJson.version;
+
+const repo = git(repoLocation);
+
+// console.log(repo)
+
+repo.current_commit((err, commits) => {
+    if (err)
+        console.error(err)
+    else
+        console.log(commits)
+})
+
+// const repo = git.Git(repoLocation);
+
+// console.log({repo, version})
 
